6996e4210e71ca70e5ed703bc50b39c92143f975
Ryan <ryan.tjong888@gmail.com>
2020-09-12 03:22:05 -0400

    updated CSS/html/js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const gym = {}
 
+// create back, chest, etc array
 
 
+// Create a scroll down and up function
 gym.scroll = () => {
     // scroll down/up code taken from Ahmad Yousef on codepen
     $(".start").click(function() {
@@ -22,6 +24,17 @@ gym.scroll = () => {
         }, 1800);
     });
 }
+
+// Create an event listener for a 'submit' event
+$('form').on('submit', function(event) {
+    // Prevent the html from refreshing
+    event.preventDefault();
+    console.log('are we eveen submitting');
+// Save information from checked radio buttons into variables
+const muscle = $('input[name=muscle]:checked').val();
+const goal = $('input[name=goal]:checked').val();
+console.log(muscle, goal);
+});
 
 gym.init = () => {
     gym.scroll();
